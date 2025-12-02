1259d0ed18b3adfdf7cc65ce98631c84ccf8681f
Alexxxxxxxxxxxxy <3092469493@qq.com>
2025-12-02 23:34:54 +0800

    add not found page

--- admin/src/pages/NotFound.jsx
+++ admin/src/pages/NotFound.jsx
@@ -4,8 +4,8 @@ import notFoundImage from '../assets/404.png'
 const NotFound = () => {
   return (
     <div className='flex flex-col justify-center items-center w-full h-full mt-2'>
-      <p className='sm:text-4xl text-2xl noto-serif-sc mb-5'>未找到页面</p>
-      <img className='sm:w-[500px] w-[200px]' src={notFoundImage} alt="404" />
+        <p className='sm:text-4xl text-2xl noto-serif-sc mb-5'>未找到页面</p>
+        <img className='sm:w-[500px] w-[200px]' src={notFoundImage} alt="404" />
     </div>
   )
 }
